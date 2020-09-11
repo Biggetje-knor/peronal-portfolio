1359d95132ab1ac496337cc45fe8168b40e28577
Harold de Jong <hrjong@ziggo.nl>
2020-09-11 14:13:00 +0000

    2e met gitignore

--- portfo_site/settings.py
+++ portfo_site/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'lr(za$i^ys88(^!tnp!3li)-h$oyt+tk5+sughb@z%$=2#7k17'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['harolddejong.pythonanywhere.com']
 
 
 # Application definition
@@ -120,5 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 MEDIA_URL  = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
